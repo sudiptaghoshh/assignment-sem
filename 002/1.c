184ddc05d76b6373e493e9649afe012718a3b381
Sudipta Ghosh <sudiptaghoshkrg@gmail.com>
2023-09-03 12:01:26 +0530

    2nd commit

--- 002/1.c
+++ 002/1.c
@@ -2,14 +2,14 @@
 
 int main() {
 
-    char ch ='*';
+    char ch = '*';
     int n;
     printf("enter number : ");
-    scanf("%d",&n);
-    
-    for(int i=1;i<n+1;i++){
-        for(int j=0;j<i;j++){
-        printf("%c",ch);
+    scanf("%d", &n);
+
+    for (int i = 1;i < n + 1;i++) {
+        for (int j = 0;j < i;j++) {
+            printf("%c", ch);
         }
         printf("\n");
     }
